fac1813948663f463cd9d30b5b02665be1f87114
Crowdbotics <team@crowdbotics.com>
2023-04-23 12:23:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billcash-40138",
+  url: "https://billcash-40138.botics.co",
+  api: "https://billcash-40138.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
